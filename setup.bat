063afe3a5304ab5ee759072626a5d5827a76a111
Tim Ansell <tim@thousandparsec.net>
2006-12-25 12:58:07 +0000

    Added update of tpclient-pywx-dev.

--- setup.bat
+++ setup.bat
@@ -41,6 +41,36 @@ xcopy /S ..\..\libtpproto-py\tp\netlib netlib
 IF NOT EXIST ..\..\libtpclient-py GOTO ERROR
 mkdir client
 xcopy /S ..\..\libtpclient-py\tp\client client
+cd ..
+cd ..
+
+rem Update everything
+IF NOT EXIST libtpclient-py-dev GOTO SKIP_UPDATE_TPCLIENT_DEV
+cd libtpclient-py-dev
+darcs pull -a
+cd ..
+:SKIP_UPDATE_TPCLIENT_DEV
+
+rem Setup the tpclient-pywx for inplace operation
+cd tpclient-pywx-dev
+darcs pull -a
+
+IF NOT EXIST tp GOTO SKIP_DELETE
+DEL /S /Q /F tp
+:SKIP_DELETE
+
+mkdir tp
+cd tp
+echo # > __init__.py
+IF NOT EXIST ..\..\libtpproto-py GOTO ERROR
+mkdir netlib
+xcopy /S ..\..\libtpproto-py\tp\netlib netlib
+
+IF NOT EXIST ..\..\libtpclient-py-dev GOTO ERROR
+mkdir client
+xcopy /S ..\..\libtpclient-py-dev\tp\client client
+cd ..
+cd ..
 
 exit
 
