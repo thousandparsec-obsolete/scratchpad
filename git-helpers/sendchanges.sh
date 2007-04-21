e3cf0d07eb143489c3e795a80bb5ed796b6e680a
Tim Ansell <mithro@mithis.com>
2007-04-22 00:25:59 +0930

    sendchanges.sh could possibly work now.

--- git-helpers/sendchanges.sh
+++ git-helpers/sendchanges.sh
@@ -4,12 +4,12 @@ DATE=`date +%Y%m%d-%H%M`
 
 for DIR in ls *; do
 	if [ -d $DIR ]; then
-		if [ -d $DIR/_darcs ]; then
+		if [ -d $DIR/.git ]; then
 			echo
 			echo $DIR
 			echo "------------------------------------------"
 			cd $DIR
-			darcs send http://darcs.thousandparsec.net/repos/$DIR -o ~/$DIR.patch.$DATE
+			git format-patch --keep-subject origin > ~/$DIR.patch.$DATE
 			cd ..
 			echo "------------------------------------------"
 		fi
