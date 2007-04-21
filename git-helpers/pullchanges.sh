dc55cc15841895ea0f3710e09dd9c6c78d1b4eb7
Tim Ansell <tim@ultraslim.(none)>
2007-04-21 23:50:03 +0930

    Pull changes should now work.
    FIXME: Should pull all branches - not just the default one.

--- git-helpers/pullchanges.sh
+++ git-helpers/pullchanges.sh
@@ -2,12 +2,12 @@
 
 for DIR in ls *; do
 	if [ -d $DIR ]; then
-		if [ -d $DIR/_darcs ]; then
+		if [ -d $DIR/.git ]; then
 			echo
 			echo $DIR
 			echo "------------------------------------------"
 			cd $DIR
-			darcs pull
+			cg-fetch
 			cd ..
 			echo "------------------------------------------"
 		fi
