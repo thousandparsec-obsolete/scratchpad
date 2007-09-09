68c5bdabfa7e2041732bfae232659c994c37aa43
Tim Ansell <mithro@mithis.com>
2007-09-09 11:46:49 +0930

    Added ability to setup tpsai-py for inplace operation.

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ cd ..
 
 # Setup the Python Server for inplace operation
 if [ -e tpserver-py ]; then
+	echo "Setting up tpserver-py for inplace operation..."
 	cd tpserver-py
 
 	cd tp
@@ -31,6 +32,7 @@ fi
 
 # Setup the tpclient-pywx for inplace operation
 if [ -e tpclient-pywx ]; then
+	echo "Setting up tpclient-pywx stable branch for inplace operation..."
 	cd tpclient-pywx
 
 	chmod a+x tpclient-pywx
@@ -55,7 +57,7 @@ if [ -e tpclient-pywx ]; then
 fi
 
 if [ -e tpclient-pywx-dev ]; then
-	echo "Setting up tpclient-pywx Development Branch"
+	echo "Setting up tpclient-pywx development branch for inplace operation..."
 	cd tpclient-pywx-dev
 
 	chmod a+x tpclient-pywx
@@ -70,4 +72,34 @@ if [ -e tpclient-pywx-dev ]; then
 	fi
 	ln -fs ../../libtpproto-py/tp/netlib netlib
 	ln -fs ../../libtpclient-py-dev/tp/client client
+	cd ..
+
+	cd ..
+fi
+
+
+# Setup the tpsai-py for inplace operation
+if [ -e tpsai-py ]; then
+	echo "Setting up tpsai-py for inplace operation..."
+	cd tpsai-py
+
+	chmod a+x tpsai-py
+
+	if [ -e tp ]; then
+		rm -rf tp
+	fi
+	mkdir tp
+	cd tp
+	if [ ! -e __init__.py ]; then
+		touch __init__.py
+	fi
+	if [ ! -e netlib ]; then
+		ln -s ../../libtpproto-py/tp/netlib netlib
+	fi
+	if [ ! -e client ]; then
+		ln -s ../../libtpclient-py/tp/client client
+	fi
+	cd ..
+
+	cd ..
 fi
