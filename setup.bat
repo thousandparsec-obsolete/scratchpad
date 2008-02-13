b7e6803c41313c127896b9547c548dcaba278709
Tim Ansell <mithro@mithis.com>
2008-02-13 19:58:15 +1030

    Fix the setup.bat for using git (it still referenced darcs!).

--- setup.bat
+++ setup.bat
@@ -6,71 +6,39 @@ cd ..
 rem Update everything
 IF NOT EXIST libtpproto-py GOTO SKIP_UPDATE_TPPROTO
 cd libtpproto-py
-darcs pull -a
+git pull --verbose origin
+python setup.py develop
 cd ..
 :SKIP_UPDATE_TPPROTO
 
 IF NOT EXIST libtpclient-py GOTO SKIP_UPDATE_TPCLIENT
 cd libtpclient-py
-darcs pull -a
+git pull origin
+python setup.py develop
 cd ..
 :SKIP_UPDATE_TPCLIENT
 
 rem Setup the Python Server for inplace operation
 IF NOT EXIST tpserver-py GOTO SKIP_SERVER
 cd tpserver-py
-darcs pull -a
+git pull origin
 cd ..
 :SKIP_SERVER
 
 rem Setup the tpclient-pywx for inplace operation
 cd tpclient-pywx
-darcs pull -a
+git pull origin
 
 IF NOT EXIST tp GOTO SKIP_DELETE
 DEL /S /Q /F tp
 :SKIP_DELETE
 
-mkdir tp
-cd tp
-echo # > __init__.py
-IF NOT EXIST ..\..\libtpproto-py GOTO ERROR
-mkdir netlib
-xcopy /S ..\..\libtpproto-py\tp\netlib netlib
-
-IF NOT EXIST ..\..\libtpclient-py GOTO ERROR
-mkdir client
-xcopy /S ..\..\libtpclient-py\tp\client client
-cd ..
 cd ..
 
-rem Update everything
-IF NOT EXIST libtpclient-py-dev GOTO SKIP_UPDATE_TPCLIENT_DEV
-cd libtpclient-py-dev
-darcs pull -a
-cd ..
-:SKIP_UPDATE_TPCLIENT_DEV
 
-rem Setup the tpclient-pywx for inplace operation
-cd tpclient-pywx-dev
-darcs pull -a
+rem We no longer need to do the song and dance as ez_setup finally works properly!
 
-IF NOT EXIST tp GOTO SKIP_DELETE
-DEL /S /Q /F tp
-:SKIP_DELETE
-
-mkdir tp
-cd tp
-echo # > __init__.py
-IF NOT EXIST ..\..\libtpproto-py GOTO ERROR
-mkdir netlib
-xcopy /S ..\..\libtpproto-py\tp\netlib netlib
-
-IF NOT EXIST ..\..\libtpclient-py-dev GOTO ERROR
-mkdir client
-xcopy /S ..\..\libtpclient-py-dev\tp\client client
-cd ..
-cd ..
+pause
 
 exit
 
